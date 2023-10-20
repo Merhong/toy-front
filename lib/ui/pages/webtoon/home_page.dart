70d105a3f067a4b99425c2017f733c9be5278374
YHYELIM <yhl0513@naver.com>
2023-10-20 17:31:56 +0900

    homewpage 진행중

--- lib/ui/pages/webtoon/home_page.dart
+++ lib/ui/pages/webtoon/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_blog/data/model/webtoon.dart';
-import 'package:flutter_blog/data/repository/webtoon_repository.dart';
+import 'package:flutter_blog/widget/home_list_body.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -9,41 +8,7 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Webtoons'),
       ),
-      body: FutureBuilder<List<Webtoon>>(
-        future: WebtoonRepository().findAllWebtoon(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return Center(child: Text('No data available.'));
-          } else {
-            final webtoons = snapshot.data!;
-            return ListView.separated(
-              itemCount: webtoons.length,
-              itemBuilder: (context, index) {
-                final webtoon = webtoons[index];
-                return Column(
-                  children: [
-                    // Use webtoon data here
-                    Image.network(
-                      webtoon.image,
-                      height: 300,
-                    ),
-                    Text(webtoon.title),
-                    Text(webtoon.author),
-                    Text(webtoon.starCount),
-                  ],
-                );
-              },
-              separatorBuilder: (context, index) {
-                return const Divider();
-              },
-            );
-          }
-        },
-      ),
+      body: HomeListBody(),
     );
   }
 }
--- test/main2_test.dart
+++ test/main2_test.dart
@@ -1,18 +1,1 @@
-import 'package:flutter_blog/data/model/webtoon.dart';
-import 'package:flutter_blog/data/repository/webtoon_repository.dart';
-import 'package:flutter_test/flutter_test.dart';
 
-void main() {
-  test('Print Webtoon List', () async {
-    WebtoonRepository repository = WebtoonRepository();
-    List<Webtoon> webtoons = await repository.findAll();
-
-    for (Webtoon webtoon in webtoons) {
-      print('Image: ${webtoon.image}');
-      print('Title: ${webtoon.title}');
-      print('Author: ${webtoon.author}');
-      print('Star Count: ${webtoon.starCount}');
-      print('-------------------');
-    }
-  });
-}
